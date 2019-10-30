92727d08a0ccc959867f714937011995ef477cf2
KYD <kydkang@gmail.com>
2019-10-30 17:01:34 -0500

    Add download

--- commons/views.py
+++ commons/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
 
-# Create your views here.
+import csv
+
+from django.http import HttpResponse
+from django.contrib.auth.models import User
+
+
+def export_users_csv(request):
+    response = HttpResponse(content_type='text/csv')
+    response['Content-Disposition'] = 'attachment; filename="users.csv"'
+
+    writer = csv.writer(response)
+    writer.writerow(['Username', 'First name', 'Last name', 'Email address'])
+
+    users = User.objects.all().values_list('username', 'first_name', 'last_name', 'email')
+    for user in users:
+        writer.writerow(user)
+
+    return response
