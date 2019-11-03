cca3bae8d0d8a80a53b5dc463c1b22de833c5804
KYD <kydkang@gmail.com>
2019-11-03 14:31:52 -0500

    Add clean() in the models.py

--- sims101/models.py
+++ sims101/models.py
@@ -3,6 +3,7 @@ from django.urls import reverse
 from django.utils.translation import gettext_lazy as _
 from commons.models import Description
 from django.shortcuts import get_object_or_404
+from django.core.exceptions import ValidationError
 
 class Index101(models.Model):       
     SEQUENCE = '101'
@@ -27,9 +28,14 @@ class Index101(models.Model):
         # self.description = Description.objects.get(sequence=self.SEQUENCE)
         self.description = get_object_or_404(Description, sequence=self.SEQUENCE)
         super(Index101, self).save(*args, **kwargs)
+    
+    def clean(self):
+        if self.data_one > self.data_three or self.data_two > self.data_three: 
+            raise ValidationError(_('Entered numbers are not correct.  Try again.'))
 
     def __str__(self):
         return str(self.data_one) + ",    " + str(self.data_two)
+
 
     # def get_absolute_url(self):
     #     # or  return reverse('sims101:index_detail', args=[str(self.id)])
