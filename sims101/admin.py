b37567aaa25d2e196df551ac378f11aa20472dbe
KYD <kydkang@gmail.com>
2019-11-11 13:57:58 -0500

    In admin make the calculated_value read-only

--- sims101/admin.py
+++ sims101/admin.py
@@ -1,6 +1,16 @@
 from django.contrib import admin
 from .models import Index101, Index101Data
 
-admin.site.register(Index101)
 
+
+class Index101Admin(admin.ModelAdmin):
+#     # exclude=("calculated_value",)
+    readonly_fields=('calculated_value', )
+
+admin.site.register(Index101, Index101Admin)
+
+
+# admin.site.register(Index101)
 admin.site.register(Index101Data)
+
+
